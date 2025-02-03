32c01a275a16ae722da52d440bb54ee65778e60d
Christian Marius Lillelund <chr1000@gmail.com>
2025-02-03 19:56:11 +0100

    Set batch_size=n_samples

--- src/experiments/train_semisynthetic_datasets.py
+++ src/experiments/train_semisynthetic_datasets.py
@@ -129,7 +129,7 @@ if __name__ == "__main__":
             copula = Frank_Bivariate(2.0, 1e-4, dtype=dtype, device=device)
         dep_model1, dep_model2, copula, min_val_loss = train_copula_model(dep_model1, dep_model2, train_dict,
                                                                           valid_dict, copula=copula, n_epochs=100000,
-                                                                          patience=100, lr=0.001, batch_size=1024,
+                                                                          patience=100, lr=0.001, batch_size=n_samples,
                                                                           copula_name=copula_name, verbose=True)
         copula_theta = float(copula.parameters()[0][0])
         k = sum(param.numel() for param in dep_model1.parameters())
