90ed53896594dbe9ec5044bc956d424d5dbe8d68
Christian Marius Lillelund <chr1000@gmail.com>
2025-02-04 17:22:06 +0100

    Updated synthetic script

--- src/experiments/train_synthetic_datasets.py
+++ src/experiments/train_synthetic_datasets.py
@@ -39,7 +39,7 @@ data_cfg = {
 SEEDS = list(range(0, 10))
 COPULA_NAMES = ["clayton", "frank"]
 K_TAU = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
-DATA = [(5000, 10)]
+DATA = [(1000, 10), (5000, 10), (10000, 10)]
 LINEAR = True
 
 if __name__ == "__main__":
@@ -123,18 +123,20 @@ if __name__ == "__main__":
                                                        alpha=theta)
                     ci_dep_ipcw = dep_evaluator.concordance(method="IPCW")[0]
                     ibs_dep_bg = dep_evaluator.integrated_brier_score(method="BG", num_points=10)
-                    #ibs_dep_ipcw = dep_evaluator.integrated_brier_score(method="IPCW", num_points=10)
+                    ibs_dep_ipcw = dep_evaluator.integrated_brier_score(method="IPCW", num_points=10)
                     mae_dep_bg = dep_evaluator.mae(method="BG")
-                    #mae_dep_ipcw = dep_evaluator.mae(method="IPCW")
+                    mae_dep_ipcw = dep_evaluator.mae(method="IPCW")
                     
                     # Calculate errors
                     ci_harrell_error = abs(ci_true - ci_harrell)
                     ci_dep_ipcw_error = abs(ci_true - ci_dep_ipcw)
                     ci_uno_error = abs(ci_true - ci_uno)
                     ibs_ipcw_error = abs(ibs_true - ibs_ipcw)
-                    ibs_dep_error = abs(ibs_true - ibs_dep_bg)
+                    ibs_dep_bg_error = abs(ibs_true - ibs_dep_bg)
+                    ibs_dep_ipcw_error = abs(ibs_true - ibs_dep_ipcw)
                     mae_margin_error = abs(mae_true - mae_margin)
-                    mae_dep_error = abs(mae_true - mae_dep_bg)
+                    mae_dep_bg_error = abs(mae_true - mae_dep_bg)
+                    mae_dep_ipcw_error = abs(mae_true - mae_dep_ipcw)
                     
                     # Store results in the dictionary
                     results_dict[(seed, copula_name, k_tau, n_samples, n_features)] = {
@@ -142,9 +144,11 @@ if __name__ == "__main__":
                         "ci_dep_ipcw_error": ci_dep_ipcw_error,
                         "ci_uno_error": ci_uno_error,
                         "ibs_ipcw_error": ibs_ipcw_error,
-                        "ibs_dep_error": ibs_dep_error,
+                        "ibs_dep_bg_error": ibs_dep_bg_error,
+                        "ibs_dep_ipcw_error": ibs_dep_ipcw_error,
                         "mae_margin_error": mae_margin_error,
-                        "mae_dep_error": mae_dep_error
+                        "mae_dep_bg_error": mae_dep_bg_error,
+                        "mae_dep_ipcw_error": mae_dep_ipcw_error
                     }
                     
             # Flatten the nested dictionary into a list of rows
@@ -164,4 +168,5 @@ if __name__ == "__main__":
             # Save results to a CSV file
             filename = f"{cfg.RESULTS_DIR}/synthetic_results.csv"
             results_df.to_csv(filename, index=False)
+        
         
