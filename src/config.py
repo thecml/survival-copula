307a3accbeca9419665bef4c9455b1a3a85f484f
Christian Marius Lillelund <chr1000@gmail.com>
2025-02-05 20:04:06 +0100

    Updated gbsa, rsf params

--- src/config.py
+++ src/config.py
@@ -17,8 +17,8 @@ GBSA_PARAMS = {
     'learning_rate': 0.1,
     'max_depth': 1,
     'loss': 'coxph',
-    'min_samples_split': 10,
-    'min_samples_leaf': 5,
+    'min_samples_split': 2,
+    'min_samples_leaf': 1,
     'max_features': 'sqrt',
     'subsample': 0.8,
     'seed': 0,
@@ -28,8 +28,8 @@ GBSA_PARAMS = {
 RSF_PARAMS = {
     'n_estimators': 100,
     'max_depth': 1,
-    'min_samples_split': 10,
-    'min_samples_leaf': 5,
+    'min_samples_split': 2,
+    'min_samples_leaf': 1,
     'max_features': 'sqrt',
     "random_state": 0
 }
