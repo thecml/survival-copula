aea4444222113b76f855700988739975d78fd9bf
thecml <chr1000@gmail.com>
2025-06-27 10:00:50 -0300

    Implemented CG in python

--- src/metrics.py
+++ src/metrics.py
@@ -25,33 +25,128 @@ pandas2ri.activate()
 
 compound_cox = importr("compound.Cox")
 
-class CopulaGraphic():
-    def __init__(self, event_times, event_indicators,
+"""
+Courtesy of https://github.com/shi-ang/SurvivalEVAL/blob/main/SurvivalEVAL/NonparametricEstimator/SingleEvent.py#L281
+"""
+@dataclass
+class CopulaGraphic:
+    """
+    Implementation of the Copula Graphic estimator for survival function, under the dependent censoring assumption.
+
+    This implementation supports three types of copulas:
+    - Clayton
+    - Gumbel
+    - Frank
+    The estimator is based on the R code in the package 'compound.Cox' by Takeshi Emura.
+    To see the original R code, visit:
+    https://github.com/cran/compound.Cox/tree/master/R/CG.Clayton.R
+    https://github.com/cran/compound.Cox/tree/master/R/CG.Gumbel.R
+    https://github.com/cran/compound.Cox/tree/master/R/CG.Frank.R
+
+    However, the original R code (and also the math derivation) cannot handle ties --
+    e.g., when a censored instance and an event instance have the same time point.
+    This implementation correctly handling ties.
+    based on the derivation in paper: https://arxiv.org/abs/2502.19460
+    """
+    event_times: InitVar[np.array]
+    event_indicators: InitVar[np.array]
+    alpha: InitVar[float]
+    type: InitVar[str] = "clayton"
+    n_samples: int = field(init=False)
+    survival_times: np.array = field(init=False)
+    population_count: np.array = field(init=False)
+    events: np.array = field(init=False)
+    survival_probabilities: np.array = field(init=False)
+
+    def __post_init__(self, event_times, event_indicators, alpha, type):
+        alpha = max(alpha, 1e-9)
+        self.n_samples = len(event_times)
+        index = np.lexsort((event_indicators, event_times))
+        unique_times = np.unique(event_times[index], return_counts=True)
+        self.survival_times = unique_times[0]
+        self.population_count = np.flip(np.flip(unique_times[1]).cumsum())
+
+        event_counter = np.append(0, unique_times[1].cumsum()[:-1])
+        event_ind = list()
+        for i in range(np.size(event_counter[:-1])):
+            event_ind.append(event_counter[i])
+            event_ind.append(event_counter[i + 1])
+        event_ind.append(event_counter[-1])
+        event_ind.append(len(event_indicators))
+        self.events = np.add.reduceat(np.append(event_indicators[index], 0), event_ind)[::2]
+
+        event_diff = self.population_count - self.events
+
+        with np.errstate(divide="ignore"):
+            # ignore division by zero warnings,
+            # such warnings are expected when the last time point has an event so the event_diff is 0.
+            # but we will set the last point to 0 anyway.
+            if type == "clayton":
+                diff_ = (event_diff / self.n_samples) ** (- alpha) - (self.population_count / self.n_samples) ** (- alpha)
+                diff_[-1] = 0
+                self.survival_probabilities = (1.0 + np.cumsum(diff_)) ** ( - 1.0 / alpha)
+            elif type == "gumbel":
+                diff_ = ((- np.log(event_diff / self.n_samples)) ** (alpha + 1) -
+                         (-np.log(self.population_count / self.n_samples)) ** (alpha + 1))
+                diff_[-1] = 0
+                self.survival_probabilities = np.exp(  -np.cumsum(diff_) ** (1 / (1 + alpha))  )
+            elif type == "frank":
+                log_diff_ = np.log(  (np.exp(-alpha * event_diff / self.n_samples) - 1) / (np.exp(-alpha * self.population_count / self.n_samples) - 1)  )
+                log_diff_[-1] = 0
+                self.survival_probabilities = -1 / alpha * np.log(  1 + (np.exp(-alpha) - 1) * np.exp(np.cumsum(log_diff_))  )
+            else:
+                raise ValueError(f"Unknown copula type: {type}. Supported types are 'clayton', 'gumbel', and 'frank'.")
+
+        self.cumulative_dens = 1 - self.survival_probabilities
+        self.probability_dens = np.diff(np.append(self.cumulative_dens, 1))
+
+    def predict(self, prediction_times: np.array) -> np.array:
+        """
+        Predict the survival probabilities at the given prediction times.
+        Parameters
+        ----------
+        prediction_times: np.array
+            The times at which to predict the survival probabilities.
+        Returns
+        -------
+        np.array
+            The predicted survival probabilities at the given times.
+        """
+        probability_index = np.digitize(prediction_times, self.survival_times)
+        probability_index = np.where(
+            probability_index == self.survival_times.size + 1,
+            probability_index - 1,
+            probability_index,
+        )
+        probabilities = np.append(1, self.survival_probabilities)[probability_index]
+
+        return probabilities
+
+    def predict_median(self) -> float:
+        """
+        Predict the median survival time based on the survival probabilities.
+        It is calculated as the first time point where the survival probability is less than or equal to 0.5.
+        No interpolation is performed, as we are strictly following the implementation of the CG estimator in R.
+        Returns
+        -------
+        float
+            The predicted median survival time.
+        """
+        median_index = np.where(self.survival_probabilities <= 0.5)[0]
+        if median_index.size == 0:
+            return np.inf
+        return self.survival_times[median_index[0]]
+
+class CopulaGraphicWrapper():
+    def __init__(self, time_bins, event_times, event_indicators,
                  copula_name="clayton", alpha=0) -> None:
         #index = np.lexsort((event_indicators, event_times))
         #unique_times = np.unique(event_times[index], return_counts=True)
         #self.survival_times = unique_times[0]
         
-        if copula_name == "clayton":
-            cg_result = compound_cox.CG_Clayton(event_times,
-                                                event_indicators,
-                                                alpha=alpha,
-                                                S_plot=False)
-        elif copula_name == "frank":
-            cg_result = compound_cox.CG_Frank(event_times,
-                                              event_indicators,
-                                              alpha=alpha,
-                                              S_plot=False)
-        elif copula_name == "gumbel":
-            cg_result = compound_cox.CG_Gumbel(event_times,
-                                               event_indicators,
-                                               alpha=alpha,
-                                               S_plot=False)
-        else:
-            raise NotImplementedError()
-        
-        self.survival_probabilities = cg_result.rx2('surv')
-        self.survival_times = cg_result.rx2('time')
+        cg_estimator = CopulaGraphic(event_times, event_indicators, alpha=alpha, type=copula_name)
+        self.survival_probabilities = cg_estimator.predict(time_bins)
+        self.survival_times = time_bins
         
         #area_probabilities = np.append(1, self.survival_probabilities)
         #area_times = np.append(0, self.survival_times)
@@ -207,8 +302,9 @@ class DependentEvaluator:
         train_event_indicators = train_event_indicators.astype(bool)
         
         if method == "BG":
-            cg_model = CopulaGraphic(train_event_times, train_event_indicators,
-                                     copula_name=copula_name, alpha=alpha)
+            time_bins = self.time_coordinates
+            cg_model = CopulaGraphicWrapper(time_bins, train_event_times, train_event_indicators,
+                                            copula_name=copula_name, alpha=alpha)
             cg_linear_zero = cg_model.cg_linear_zero
             if np.isinf(cg_linear_zero):
                 cg_linear_zero = max(cg_model.survival_times)
@@ -228,8 +324,9 @@ class DependentEvaluator:
             cindex, concordant_pairs, discordant_pairs, risk_ties, time_ties = estimate_concordance_index(
                 event_indicators, event_times, estimate=risks, bg_event_time=bg_event_times, partial_weights=partial_weights)
         elif method == "IPCW":
-            cg_model_event = CopulaGraphic(train_event_times, train_event_indicators,
-                                           copula_name=copula_name, alpha=alpha)
+            time_bins = self.time_coordinates
+            cg_model_event = CopulaGraphicWrapper(time_bins, train_event_times, train_event_indicators,
+                                                  copula_name=copula_name, alpha=alpha)
             cg_linear_zero = cg_model_event.cg_linear_zero
             if np.isinf(cg_linear_zero):
                 cg_linear_zero = max(cg_model_event.survival_times)
@@ -244,8 +341,9 @@ class DependentEvaluator:
                 event_times_mask = event_times[mask]
             
             inverse_train_event_indicators = 1 - train_event_indicators
-            cg_model_censor = CopulaGraphic(train_event_times, inverse_train_event_indicators,
-                                            copula_name=copula_name, alpha=alpha)
+            time_bins = self.time_coordinates
+            cg_model_censor = CopulaGraphicWrapper(time_bins, train_event_times, inverse_train_event_indicators,
+                                                   copula_name=copula_name, alpha=alpha)
             ipcw_test = cg_model_censor.predict(event_times_mask)
             
             if tau is None:
@@ -309,8 +407,9 @@ class DependentEvaluator:
 
         if method == "BG":
             censored_times = event_times[event_indicators == 0]
-            cg_model = CopulaGraphic(train_event_times, train_event_indicators,
-                                     copula_name=copula_name, alpha=alpha)
+            time_bins = self.time_coordinates
+            cg_model = CopulaGraphicWrapper(time_bins, train_event_times, train_event_indicators,
+                                            copula_name=copula_name, alpha=alpha)
 
             censored_times_bg = cg_model.best_guess(censored_times)
             event_times_bg = event_times.copy()
@@ -336,8 +435,9 @@ class DependentEvaluator:
             # Use the CG estimator for IPCW
             #ipc_model = CopulaGraphic(train_event_times, inverse_train_event_indicators,
             #                          copula_name=copula_name, alpha=alpha)
-            ipc_model = CopulaGraphic(train_event_times, inverse_train_event_indicators,
-                                      copula_name=copula_name, alpha=alpha)
+            time_bins = self.time_coordinates
+            ipc_model = CopulaGraphicWrapper(time_bins, train_event_times, inverse_train_event_indicators,
+                                             copula_name=copula_name, alpha=alpha)
 
             # Category one calculates IPCW weight at observed time point.
             # Category one is individuals with event time lower than the time of interest and were NOT censored.
@@ -396,8 +496,9 @@ class DependentEvaluator:
         
         # Calculate the weighting for each sample
         if method in ["BG", "IPCW"]:
-            cg_model = CopulaGraphic(train_event_times, train_event_indicators,
-                                     copula_name=copula_name, alpha=alpha)
+            time_bins = self.time_coordinates
+            cg_model = CopulaGraphicWrapper(time_bins, train_event_times, train_event_indicators,
+                                            copula_name=copula_name, alpha=alpha)
             cg_linear_zero = cg_model.cg_linear_zero
             if np.isinf(cg_linear_zero):
                 cg_linear_zero = max(cg_model.survival_times)
