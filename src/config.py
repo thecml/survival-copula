41dd626c3fae95eb60e03180de2be42e4ba100f8
thecml <chr1000@gmail.com>
2025-09-02 12:46:52 +0200

    Set using nonlinear, updated script

--- src/config.py
+++ src/config.py
@@ -15,7 +15,7 @@ COXPH_PARAMS = {
 GBSA_PARAMS = {
     'n_estimators': 100,
     'learning_rate': 0.1,
-    'max_depth': 1,
+    'max_depth': 3,
     'loss': 'coxph',
     'min_samples_split': 2,
     'min_samples_leaf': 1,
@@ -27,7 +27,7 @@ GBSA_PARAMS = {
 
 RSF_PARAMS = {
     'n_estimators': 100,
-    'max_depth': 1,
+    'max_depth': 3,
     'min_samples_split': 2,
     'min_samples_leaf': 1,
     'max_features': 'sqrt',
--- src/models.py
+++ src/models.py
@@ -238,7 +238,7 @@ class Weibull_log_linear:
     
 class Weibull_model(nn.Module):
     def __init__(self, n_features, hidden_units=32,
-                 non_linear=False, device="cpu",
+                 non_linear=True, device="cpu",
                  dtype=torch.float64):
         super(Weibull_model, self).__init__()
         self.device = device
@@ -253,12 +253,14 @@ class Weibull_model(nn.Module):
         if self.non_linear:
             self.net = nn.Sequential(
                 nn.Linear(n_features, hidden_units, device=device, dtype=dtype),
+                nn.BatchNorm1d(hidden_units),
                 nn.ReLU(),
                 nn.Linear(hidden_units, 1, device=device, dtype=dtype)
             )
         else:
             self.net = nn.Sequential(
                 nn.Linear(n_features, hidden_units, device=device, dtype=dtype),
+                nn.BatchNorm1d(hidden_units),
                 nn.Linear(hidden_units, 1, device=device, dtype=dtype)
             )
 
